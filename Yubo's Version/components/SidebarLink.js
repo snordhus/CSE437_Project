5540242f637658deb0193dac6842b00faa93a5e0
griffintlorimer <g.lorimer@wustl.edu>
2022-10-15 13:46:27 -0500

    added in a login page

--- Yubo's Version/components/SidebarLink.js
+++ Yubo's Version/components/SidebarLink.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
+
 
 function SidebarLink({Icon, text, active}) {
+  if (text == 'User') {
+    return(
+    <div className={`text-black flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation ${active && "font-bold"}`}>
+    <Icon className="h-7"/>
+    <Link href="/login" className="hidden xl:inline">
+          <a >User</a>
+    </Link>
+  </div>
+    )} else {
   return (
     <div className={`text-black flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation ${active && "font-bold"}`}>
-      <Icon classname="h-7"/>
+      <Icon className="h-7"/>
       <span className="hidden xl:inline">{text}</span>
     </div>
   )
+  }
 }
 
 export default SidebarLink
---
+++ Yubo's Version/pages/login.js
@@ -0,0 +1,10 @@
+import LoginForum from "../components/LoginForum"
+import React from "react"
+
+function Login(){
+    return(
+         <LoginForum /> 
+        )
+}
+
+export default Login
